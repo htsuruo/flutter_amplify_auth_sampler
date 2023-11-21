9ee3477034991ca073a13eba5aca4bfe55338434
htsuruo <tsuru.dev@gmail.com>
2023-11-21 22:26:01 +0900

    Initialize Amplify Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'amplifyconfiguration.dart';
 
 void main() {
   runApp(
@@ -9,15 +13,38 @@ void main() {
   );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+// Initialize Amplify Auth
+// ref. https://docs.amplify.aws/flutter/build-a-backend/auth/set-up-auth/#initialize-amplify-auth
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  Future<void> _configureAmplify() async {
+    try {
+      final auth = AmplifyAuthCognito();
+      await Amplify.addPlugin(auth);
+      await Amplify.configure(amplifyconfig);
+    } on Exception catch (e) {
+      safePrint('An error occurred configuring Amplify: $e');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text(''),
         ),
       ),
     );
